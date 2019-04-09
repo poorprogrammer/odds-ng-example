e138c90989bbe431d141789968e8ba24b24621ff
jane <jane@odd-e.comjane@odd-e.com>
2019-04-10 00:09:19 +0700

    Add example for communicate from parent ot cheild using input:

--- README.md
+++ README.md
@@ -5,8 +5,11 @@ Use as an example and training Angular
 ## TODO 
 
 ## Commucating between components
+
 ### Component to component
 - [ ] BehaviorSubject
 - [ ] Simple properties
 
+### Parent to Child 
+- [x] Sharing Data via Input ใช้งาน @Input ก็ดูอธิบายง่ายดี เช่น Commponent นี้รับ input อะไรบ้าง 
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+// import { Parent } from './parent-to-child/using-input/parent/parent.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'odds-ng-example';
