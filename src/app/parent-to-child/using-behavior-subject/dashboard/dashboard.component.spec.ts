2dbe41b637b46f38d2791dcb609266d1f6fd8b0a
tumit <tumit@live.com>
2019-04-10 13:40:21 +0700

    fix(test): fix error on tests

--- src/app/parent-to-child/using-behavior-subject/dashboard/dashboard.component.spec.ts
+++ src/app/parent-to-child/using-behavior-subject/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,9 +9,10 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [DashboardComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/parent-to-child/using-behavior-subject/services/progress.service.spec.ts
+++ src/app/parent-to-child/using-behavior-subject/services/progress.service.spec.ts
@@ -3,10 +3,42 @@ import { TestBed } from '@angular/core/testing';
 import { ProgressService } from './progress.service';
 
 describe('ProgressService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let service: ProgressService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(ProgressService);
+  });
 
   it('should be created', () => {
-    const service: ProgressService = TestBed.get(ProgressService);
     expect(service).toBeTruthy();
   });
+
+  it('should have 0 progress on init', () => {
+    service.getProgress().subscribe(
+      v => expect(v).toBe(0)
+    );
+  });
+
+  it('should have 10 progress when single increase', () => {
+    service.increase();
+    service.getProgress().subscribe(
+      v => expect(v).toBe(10)
+    );
+  });
+
+  it('should have 10 progress when single decrease', () => {
+    // arrange 30
+    service.increase();
+    service.increase();
+    service.increase();
+    // act
+    service.decrease();
+    // arrange
+    service.getProgress().subscribe(
+      v => expect(v).toBe(20)
+    );
+  });
+
 });
