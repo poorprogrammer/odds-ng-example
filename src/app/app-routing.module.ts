bcb4173438fa9c3174a6ef934a3b8522cc82d25e
jane <jane@odd-e.comjane@odd-e.com>
2019-04-10 15:25:17 +0700

    rename route and output

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './parent-to-child/using-behavior-subject/dashboard/dashboard.component';
+import { ParentComponent as ParentUsingInputComponent } from './parent-to-child/using-input/parent/parent.component';
+
 
 const routes: Routes = [
-  { path: 'using-behavior-subject/dashboard', component: DashboardComponent }
+  { path: 'cbc/using-behavior-subject/dashboard', component: DashboardComponent },
+  { path: 'cbc/using-input', component: ParentUsingInputComponent }
 ];
 
 @NgModule({
